bfe88e457b3f41a7ebd44df92deabda98655bd7c
Kenvix <kenvixzure@live.com>
2018-10-20 13:50:17 +0800

    prepare spring boot

---
+++ src/main/java/com/kenvix/commenter/CommenterApplication.java
@@ -0,0 +1,12 @@
+package com.kenvix.commenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommenterApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kenvix/commenter/CommenterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kenvix.commenter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommenterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
